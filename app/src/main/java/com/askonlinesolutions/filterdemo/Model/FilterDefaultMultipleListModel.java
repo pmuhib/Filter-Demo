1dcc4f4f9c0b02f646107e9f38c1ab91afd5f408
Muhib <muhib@askonlinesolutions.com>
2018-02-20 16:31:50 +0530

    Initial commit

---
+++ app/src/main/java/com/askonlinesolutions/filterdemo/Model/FilterDefaultMultipleListModel.java
@@ -0,0 +1,25 @@
+package com.askonlinesolutions.filterdemo.Model;
+
+
+public class FilterDefaultMultipleListModel {
+
+    String name;
+    boolean checked = false;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+}
+
